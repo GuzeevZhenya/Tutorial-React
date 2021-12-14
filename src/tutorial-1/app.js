993de91e86587c50ec305bb8c3fe6665e0a27ee7
GuzeevZhenya <shyst.evgeny@mail.ru>
2021-12-14 16:26:54 +0300

    Поправил код

--- src/tutorial-1/app.js
+++ src/tutorial-1/app.js
@@ -5,8 +5,8 @@ import Classprofile from './Components/classProfile';
 export default function app() {
   return (
     <div>
-          <FuncProfile name="Вася Пупкин" registredAt={new Date(2021, 5, 22)} />
-          <Classprofile name="Вася Пупкин" registredAt={new Date(2021, 5, 22)} />
+      <FuncProfile name="Вася Пупкин" registredAt={new Date(2021, 5, 22)} />
+      <Classprofile name="Вася Пупкин" registredAt={new Date(2021, 5, 22)} />
     </div>
   );
 }
